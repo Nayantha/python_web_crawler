c1482a11a1f3c51c7dc87b6cf4807d78352d8f45
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-24 16:40:02 +0530

    add class variables to crawl spider class.

--- python_web_crawler/spiders/crawing_spider.py
+++ python_web_crawler/spiders/crawing_spider.py
@@ -1,5 +1,11 @@
-from scrapy.spiders import CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
 class CrawlingSpider(CrawlSpider):
     name = "myCrawler"
+    allowed_domains = ["vixen.com"]
+    start_url = ["https://vixen.com/videos"]
+    rules = (
+        Rule(LinkExtractor(allow="?page=")),
+    )
